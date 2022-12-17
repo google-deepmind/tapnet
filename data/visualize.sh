d1766a22d22cb77cc10bf0210c7e3a3e42d31453
yangyi02 <yangyi02@gmail.com>
2022-12-17 00:26:04 +0000

    fix merging issues

--- data/visualize.sh
+++ data/visualize.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-=======
-# Copyright 2022 DeepMind Technologies Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
->>>>>>> cb3ef0ce00547001f108aa7094a4d0f2071e4079
 
 virtualenv -p python3 /tmp/venv
 source /tmp/venv/bin/activate
--- data/visualize_pickle.py
+++ data/visualize_pickle.py
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
 # Copyright 2022 DeepMind Technologies Limited.
-=======
-# Copyright 2022 DeepMind Technologies Limited
->>>>>>> cb3ef0ce00547001f108aa7094a4d0f2071e4079
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-<<<<<<< HEAD
 #     http://www.apache.org/licenses/LICENSE-2.0
-=======
-#    http://www.apache.org/licenses/LICENSE-2.0
->>>>>>> cb3ef0ce00547001f108aa7094a4d0f2071e4079
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-<<<<<<< HEAD
-=======
-# ==============================================================================
->>>>>>> cb3ef0ce00547001f108aa7094a4d0f2071e4079
 
 """Visualize frames of a random video of the given dataset."""
 
