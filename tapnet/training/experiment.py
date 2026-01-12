6af490ddb99811e876d6c8ac9d6227757205bf7a
DeepMind <noreply@google.com>
2026-01-12 04:52:19 -0800

    Add an internal definition of `ArrayTree` and use it instead of `chex.ArrayTree`.
    PiperOrigin-RevId: 855174636
    Change-Id: I08bdea973f89b1d5b798ad311b3585f9961df159

--- tapnet/training/experiment.py
+++ tapnet/training/experiment.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -384,7 +384,7 @@ class Experiment(experiment.AbstractExperiment):
     })
     scalars = jax.lax.pmean(scalars, axis_name='i')
 
-    return params, state, opt_state, scalars
+    return params, state, opt_state, scalars  # pytype: disable=bad-return-type
 
   #                  _
   #   _____   ____ _| |
